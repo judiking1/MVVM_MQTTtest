cf26c9531ab6f77e4ba89419a55c16a20a524ea1
wonbae <judiking1@naver.com>
2023-10-07 02:20:43 +0900

    Add files via upload

---
+++ mvvmTest/mvvmTest/Model/MqttServices.cs
@@ -0,0 +1,32 @@
+﻿using MQTTnet.Client;
+using MQTTnet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mvvmTest.Model
+{
+    public class MqttServices
+    {
+        private IMqttClient _mqttClient;
+        private MqttClientOptions _mqttOptions;
+        public MqttServices()
+        {
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+        }
+        public async Task ConnectAsync()
+        {
+            // 연결 옵션 설정
+            _mqttOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer("200.100.10.11", 1883)
+                .Build();
+
+            // 브로커 연결
+            await _mqttClient.ConnectAsync(_mqttOptions);
+        }
+    }
+}
