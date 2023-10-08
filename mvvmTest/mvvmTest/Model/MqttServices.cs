6deddb8fb9e24a9df8106f666bf5bec4aebde34d
wonbae <judiking1@naver.com>
2023-10-09 03:50:53 +0900

    Add files via upload

--- mvvmTest/mvvmTest/Model/MqttServices.cs
+++ mvvmTest/mvvmTest/Model/MqttServices.cs
@@ -6,27 +6,67 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MQTTnet.Server;
 
 namespace mvvmTest.Model
 {
     public class MqttServices
     {
+        private MqttFactory mqttFactory;
         private IMqttClient _mqttClient;
         private MqttClientOptions _mqttOptions;
         public MqttServices()
         {
-            var factory = new MqttFactory();
-            _mqttClient = factory.CreateMqttClient();
+            mqttFactory = new MqttFactory();
+            _mqttClient = mqttFactory.CreateMqttClient();
         }
-        public async Task ConnectAsync()
+        public async Task ConnectAsync(string host, int port)
         {
             // 연결 옵션 설정
             _mqttOptions = new MqttClientOptionsBuilder()
-                .WithTcpServer("200.100.10.11", 1883)
+                .WithTcpServer(host, port)
                 .Build();
 
             // 브로커 연결
             await _mqttClient.ConnectAsync(_mqttOptions);
+            Console.WriteLine("MQTT Connected");
         }
+        public async Task ConnectAsync(string host)
+        {
+            // 연결 옵션 설정
+            _mqttOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(host)
+                .Build();
+
+            // 브로커 연결
+            await _mqttClient.ConnectAsync(_mqttOptions);
+            Console.WriteLine("MQTT Connected");
+        }
+
+        public async Task PublishAsync(string topic, string payload)
+        {
+            var applicationMessage = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(payload)
+            .Build();
+
+            await _mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+            Console.WriteLine("payload published");
+        }
+        public async Task SubscribeAsync(string topic)
+        {
+            var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+            .WithTopicFilter(
+                f =>
+                {
+                    f.WithTopic(topic);
+                })
+            .Build();
+
+            await _mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+            Console.WriteLine("MQTT client subscribed to topic.");
+        }
+       
     }
 }
--- mvvmTest/mvvmTest/ViewModel/MainViewModel.cs
+++ mvvmTest/mvvmTest/ViewModel/MainViewModel.cs
@@ -1,19 +1,31 @@
-﻿using mvvmTest.Utilities;
+﻿using MQTTnet.Client;
+using mvvmTest.Model;
+using mvvmTest.Utilities;
 using mvvmTest.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace mvvmTest.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private MqttServices _mqttService;
         private object _currentPage;
-
+        private readonly object _publishPage = new PublishPage();
+        private readonly object _SubscribePage = new SubscribePage();
+        public string PublishTopic { get; set; }
+        public string PublishMessage { get; set; }
+        public string SubscribeTopic { get; set; }
+        public ObservableCollection<string> TopicList { get; } = new ObservableCollection<string>();
+        public ObservableCollection<string> ReceivedMessages { get; } = new ObservableCollection<string>();
         public object CurrentPage
         {
             get { return _currentPage; }
@@ -25,12 +37,53 @@ namespace mvvmTest.ViewModel
         }
         public ICommand ShowPublishPageCommand { get; }
         public ICommand ShowSubscribePageCommand { get; }
+        public ICommand PublishCommand { get; }
+        public ICommand SubscribeCommand { get; }
 
         public MainViewModel()
         {
-            CurrentPage = new PublishPage();
-            ShowPublishPageCommand = new RelayCommand(() => CurrentPage = new PublishPage());
-            ShowSubscribePageCommand = new RelayCommand(() => CurrentPage = new SubscribePage());
+            _mqttService = new MqttServices();
+            MqttConnect();
+            CurrentPage = _publishPage;
+            ShowPublishPageCommand = new RelayCommand(() => CurrentPage = _publishPage);
+            ShowSubscribePageCommand = new RelayCommand(() => CurrentPage = _SubscribePage);
+            PublishCommand = new RelayCommand(DoPublish);
+            SubscribeCommand = new RelayCommand(DoSubscribe);
+        }
+        public async void MqttConnect()
+        {
+            try
+            {
+                await _mqttService.ConnectAsync("broker.hivemq.com");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"MQTT Connection Error: {ex.Message}");
+            }
+        }
+        private async void DoPublish()
+        {
+            if (!string.IsNullOrEmpty(PublishTopic) && !string.IsNullOrEmpty(PublishMessage))
+            {
+                await _mqttService.PublishAsync(PublishTopic, PublishMessage);
+            }
+        }
+        private async void DoSubscribe()
+        {
+            if (!string.IsNullOrEmpty(SubscribeTopic))
+            {
+                await _mqttService.SubscribeAsync(SubscribeTopic);
+                TopicList.Add(SubscribeTopic);
+            }
+        }
+        public Task HandleReceivedMessage(MqttApplicationMessageReceivedEventArgs e)
+        {
+            Console.WriteLine("Message received");
+            var receivedMessage = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            var topic = e.ApplicationMessage.Topic;
+            Console.WriteLine($"Topic: {topic}, Message: {receivedMessage}");
+
+            return Task.CompletedTask;
         }
     }
 }
